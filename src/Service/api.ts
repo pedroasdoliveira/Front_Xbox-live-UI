00e795471abfc7e154100fd286d48a58a5e483b9
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-12 23:01:27 -0300

    feat(api): add api url

---
+++ src/Service/api.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const Api = axios.create({
+    baseURL: 'https://xbox-live-production.up.railway.app/'
+})
+
+Api.interceptors.request.use((config: any) => {
+    try {
+        const token = localStorage.getItem("jwt");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    }
+    catch (error: any) {
+        console.error(error);
+    }
+})
+
+export default Api
