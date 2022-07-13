b81afb5f81b6b4b46cf7285efa528427e39f17a7
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-12 23:17:55 -0300

    feat(Login): implement auth login

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,42 +1,88 @@
 import * as Style from "./login-style";
-import XboxLogo from 'assets/icon/xbox-logo.svg'
-import Control from 'assets/img/controle_xbox.png'
+import XboxLogo from "assets/icon/xbox-logo.svg";
+import Control from "assets/img/controle_xbox.png";
 import { Link, useNavigate } from "react-router-dom";
+import { UserLogin } from "types/interfaces";
+import React, { useState } from "react";
+import { loginService } from "Service/authService";
+import swal from "sweetalert";
 
 const Login = () => {
   const navigation = useNavigate();
 
-  const handleNavigate = () => {
-    navigation('/profiles')
-  }
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChangesValues = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues((values: UserLogin) => ({
+      ...values,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleLogin = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    const response: any = await loginService.Login(values);
+
+    if (!response) {
+      swal({
+        title: 'Erro de login',
+        text: 'Usuário e/ou senha inválidos',
+        icon: 'error',
+        timer: 6000,
+      })
+    }
+
+    const jwt = response.data.token;
+
+    if (jwt) {
+      localStorage.setItem("jwt", jwt);
+      swal({
+        title: "Bem vindo",
+        icon: "success",
+        timer: 3000,
+      });
+      navigation("/profiles");
+    }
+  };
 
   return (
     <Style.Login>
       <main>
         <Style.LoginLogo src={XboxLogo} alt="Logo Xbox" />
         <Style.LoginSection>
-          <Style.LoginContent>
+          <Style.LoginContent onSubmit={handleLogin}>
             <Style.LoginImg src={Control} alt="controle xbox one" />
 
             <Style.LoginInput
               type="text"
+              name="email"
               id="email"
               placeholder=" Digite o seu E-mail de acesso"
               required
+              onChange={handleChangesValues}
             />
 
             <Style.LoginInput
               type="password"
+              name="password"
               id="password"
               placeholder=" Digite sua senha de acesso"
               required
+              onChange={handleChangesValues}
             />
 
             <Style.LoginDescription>
-              Não tem uma conta criada? <Style.LoginStrong><Link to={'/singup'}>Crie uma aqui!</Link></Style.LoginStrong>
+              Não tem uma conta criada?{" "}
+              <Style.LoginStrong>
+                <Link to={"/singup"}>Crie uma aqui!</Link>
+              </Style.LoginStrong>
             </Style.LoginDescription>
 
-            <Style.LoginButton type="submit" onClick={handleNavigate}>Login</Style.LoginButton>
+            <Style.LoginButton type="submit">Login</Style.LoginButton>
           </Style.LoginContent>
         </Style.LoginSection>
       </main>
