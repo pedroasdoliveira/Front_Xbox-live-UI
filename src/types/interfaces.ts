9a5a64b97c61623737cd73aae01d7ee94f358a8b
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-14 00:49:30 -0300

    feat(interfaces): add an object array in genders from GamesTypes

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,28 +1,30 @@
 export interface UserLogin {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export interface RegisterUser {
-    name: string;
-    nickname: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-    cpf: string;
+  name: string;
+  nickname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  cpf: string;
 }
 
 export interface GamesTypes {
-    id: string;
-    title: string;
-    coverImageUrl: string;
-    description: string;
-    year: number;
-    imbScore: number;
-    trailerYoutubeUrl: string;
-    gameplayYouTubeUrl: string;
-    genders: {
-        id: string;
-        name: string;
+  id: string;
+  title: string;
+  coverImageUrl: string;
+  description: string;
+  year: number;
+  imbScore: number;
+  trailerYoutubeUrl: string;
+  gameplayYouTubeUrl: string;
+  genders: [
+    {
+      id: string;
+      name: string;
     }
+  ];
 }
