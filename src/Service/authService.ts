e13960a553909d129f5526a4ace2d447485a9280
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-18 15:38:41 -0300

    feat(authService): remove GetGames

--- src/Service/authService.ts
+++ src/Service/authService.ts
@@ -34,19 +34,4 @@ export const RegisterService = {
   },
 };
 
-export const GetGames = {
-  AllGames: async () => {
-    try {
-      const res = await Api.get('/games');
-      return res;
-    }
-    catch (error: any) {
-      swal({
-        title: "Error",
-        text: `${error.message}`,
-        icon: "error",
-        timer: 6000,
-      });
-    }
-  }
-}
+
