4a8d9c5947c0008010d9be597a5cd34fd9aa07e0
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-18 22:36:35 -0300

    feat(ProfileService): add method POST

--- src/Service/profileService.ts
+++ src/Service/profileService.ts
@@ -1,10 +1,25 @@
 import Api from "./api";
 import swal from "sweetalert";
+import { ProfilesTypes } from "types/interfaces";
 
 export const Profiles = {
   ProfilesUser: async () => {
     try {
       const res = await Api.get("/profile", { method: "GET" });
+      return res;
+    } catch (error: any) {
+      swal({
+        title: "Error",
+        text: `${error.message}`,
+        icon: "error",
+        timer: 6000,
+      });
+    }
+  },
+
+  CreateProfile: async (profile: ProfilesTypes) => {
+    try {
+      const res = await Api.post("/profile", profile);
       return res;
     } catch (error: any) {
       swal({
