5194ad909fa754624bca2ff7dd4a4b9c8cf52ca7
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-19 21:48:00 -0300

    feat(EditProfile): implement component and styles

--- src/pages/Edit_profile/EditProfile-style.ts
+++ src/pages/Edit_profile/EditProfile-style.ts
@@ -1,19 +1,46 @@
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Background = styled.main`
-    ${({theme}) => css`
-        ${theme.mixins.BackgroundProfiles}
-    `}
+  ${({ theme }) => css`
+    ${theme.mixins.BackgroundProfiles}
+  `}
 `;
 
-export const BackIcon = styled.i`
-    ${({theme}) => css`
-        ${theme.mixins.BackIcon}
-    `}
-`
+export const EditImg = styled.img`
+  ${() => css`
+    width: 206px;
+    height: 206px;
+    border: 1px solid #0d1522;
+    border-radius: 50%;
+    cursor: pointer;
+  `}
+`;
 
-export const CreateContainer = styled.section`
-    ${({theme}) => css`
-        ${theme.mixins.ContainerCardProfile}
-    `}
+export const EditForm = styled.form`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `}
+`;
+
+export const InputEdit = styled.input`
+  ${({ theme }) => css`
+    width: 395px;
+    height: 40px;
+    border-radius: 15px;
+    border-color: #000;
+    outline: none;
+    margin: 1rem auto;
+    box-shadow: 0 4px rgba(0, 0, 0, 0.4);
+    ${theme.mixins.bodyStyle}
+  `}
+`;
+
+export const EditProfileBtn = styled.button`
+  ${({ theme }) => css`
+    ${theme.mixins.loginButton}
+    color: #000;
+  `}
 `;
--- src/pages/Edit_profile/EditProfile.tsx
+++ src/pages/Edit_profile/EditProfile.tsx
@@ -1,18 +1,45 @@
-import { useNavigate } from 'react-router-dom'
-import * as Style from './EditProfile-style'
+import ContainerForm from "components/ContainerForm";
+import ReturnPage from "components/ReturnPage";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ProfilesTypes } from "types/interfaces";
+import { Profiles } from 'Service/profileService'
+import * as Style from "./EditProfile-style";
 
 const EditProfile = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const [profile, setProfile] = useState<ProfilesTypes>();
 
   return (
     <Style.Background>
-        <Style.BackIcon onClick={() => navigate('/profiles')} />
+      <ReturnPage Route={() => navigate("/profiles")} />
 
-        <Style.CreateContainer>
-            
-        </Style.CreateContainer>
+      <ContainerForm>
+        <Style.EditImg src={profile?.imageUrl ? profile.imageUrl : ""} />
+
+        <Style.EditForm>
+          <Style.InputEdit
+            type="text"
+            placeholder=" Url da imagem"
+            name="imageUrl"
+            id="imageUrl"
+            required
+          />
+
+          <Style.InputEdit
+            type="text"
+            placeholder=" Nome do perfil"
+            name="title"
+            id="title"
+            required
+          />
+
+          <Style.EditProfileBtn>Atualizar</Style.EditProfileBtn>
+        </Style.EditForm>
+      </ContainerForm>
     </Style.Background>
-  )
-}
+  );
+};
 
-export default EditProfile
+export default EditProfile;
