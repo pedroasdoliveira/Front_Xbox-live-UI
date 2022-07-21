19b7cab8204c72961353634a46113df0d10f9cc8
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-20 22:48:27 -0300

    feat(homepageService): implement method GET by profile Id

--- src/Service/homepageService.ts
+++ src/Service/homepageService.ts
@@ -2,12 +2,17 @@ import Api from "./api";
 import swal from "sweetalert";
 
 export const Homepage = {
-    HomeByProfileId: async (id: string) => {
-        try {
-            const res = await Api.get(`/homepage/${id}`)
-        }
-        catch (error: any) {
-
-        }
+  HomeByProfileId: async (id: string) => {
+    try {
+      const res = await Api.get(`/homepage/${id}`);
+      return res;
+    } catch (error: any) {
+      swal({
+        title: "Error",
+        text: `${error.message}`,
+        icon: "error",
+        timer: 6000,
+      });
     }
-}
+  },
+};
