d819c9351fbda8e64e819b0af235398b4611792e
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-21 16:43:59 -0300

    fix(homepage): error with favoriteGames state

--- src/pages/Homepage/homepage.tsx
+++ src/pages/Homepage/homepage.tsx
@@ -1,15 +1,29 @@
 import CreateGamesAdmin from "components/Create/CreateGames";
 import ReturnPage from "components/ReturnPage";
 import { Games } from "Mocks/Games";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { HomepageTypes } from "types/interfaces";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { FavoriteGamesType } from "types/interfaces";
+import { HomepageProfile } from 'Service/homepageService'
 import * as Style from "./homepage-style";
 
 const Homepage = () => {
+  const {id} = useParams()
   const navigate = useNavigate();
 
-  const [favoriteGames, setFavoriteGames] = useState<HomepageTypes[]>([])
+  const [favoriteGames, setFavoriteGames] = useState<FavoriteGamesType[]>([]);
+
+  useEffect(() => {
+    profileFavoriteGames();
+  }, []);
+
+  const profileFavoriteGames = async () => {
+    if (id) {
+      const payloadFavorite = await HomepageProfile.HomeByProfileId(id)
+      console.log(payloadFavorite?.data);
+      setFavoriteGames(payloadFavorite?.data.profileData.favoriteGames);
+    }
+  }
 
   return (
     <Style.Homepage>
@@ -18,12 +32,15 @@ const Homepage = () => {
       <CreateGamesAdmin />
 
       <Style.CardSection>
-        {Games.map((game) => (
-          <Style.CardGame key={game.id}>
-            <Style.TitleGame>{game.title}</Style.TitleGame>
-            <Style.ScoreGame>{game.imbScore}</Style.ScoreGame>
-          </Style.CardGame>
-        ))}
+        {/* {favoriteGames.map((favorite, index) => ( // Erro: favoriteGames is not function
+          <div key={index}>
+            {favorite.games.map((game, index) => (
+              <Style.CardGame key={index}>
+                <Style.TitleGame>{game.title}</Style.TitleGame>
+              </Style.CardGame>
+            ))}
+          </div>
+        ))} */}
       </Style.CardSection>
 
       <Style.InfoSection>
