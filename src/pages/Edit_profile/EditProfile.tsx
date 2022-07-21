592a742a232e1f136b8f88f46fbb21a4890c0980
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-20 22:46:49 -0300

    feat(EditProfile): implement useParams, getProfileId with useEfecct and handleEditProfile

--- src/pages/Edit_profile/EditProfile.tsx
+++ src/pages/Edit_profile/EditProfile.tsx
@@ -1,29 +1,101 @@
 import ContainerForm from "components/ContainerForm";
 import ReturnPage from "components/ReturnPage";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import swal from "sweetalert";
 import { useNavigate } from "react-router-dom";
 import { ProfilesTypes } from "types/interfaces";
-import { Profiles } from 'Service/profileService'
+import { Profiles } from "Service/profileService";
 import * as Style from "./EditProfile-style";
 
 const EditProfile = () => {
   const navigate = useNavigate();
 
-  const [profile, setProfile] = useState<ProfilesTypes>();
+  const { id } = useParams();
+
+  const [profile, setProfile] = useState<ProfilesTypes>({
+    title: "",
+    imageUrl: "",
+    userId: "",
+  });
+
+  useEffect(() => {
+    getProfileId();
+  }, []);
+
+  const handleChangeValues = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProfile((values: ProfilesTypes) => ({
+      ...values,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const getProfileId = async () => {
+    if (id) {
+      const GetByIdProfile = await Profiles.ProfileGetById(id);
+
+      const profileValues = {
+        title: GetByIdProfile?.data.title,
+        imageUrl: GetByIdProfile?.data.imageUrl,
+        userId: GetByIdProfile?.data.userId,
+      };
+
+      setProfile({
+        ...profile,
+        ...profileValues,
+      });
+    }
+  };
+
+  const handleEditProfile = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const getUserId = localStorage.getItem("userId");
+
+    if (getUserId) {
+      setProfile({
+        ...profile,
+        userId: getUserId,
+      });
+    }
+
+    if (id) {
+      const payload: any = await Profiles.EditProfile(id, profile);
+
+      if (payload) {
+        swal({
+          title: "Perfil atualizado!",
+          icon: "success",
+          timer: 6000,
+        });
+        navigate("/profiles");
+      } 
+      else {
+        swal({
+          title: "Error",
+          text: "Erro ao atualizar o perfil!",
+          icon: "error",
+          timer: 6000,
+        });
+      }
+    }
+  };
 
   return (
     <Style.Background>
       <ReturnPage Route={() => navigate("/profiles")} />
 
       <ContainerForm>
-        <Style.EditImg src={profile?.imageUrl ? profile.imageUrl : ""} />
+        <Style.EditImg src={profile.imageUrl ? profile.imageUrl : ""} />
 
-        <Style.EditForm>
+        <Style.EditForm onSubmit={handleEditProfile}>
           <Style.InputEdit
             type="text"
             placeholder=" Url da imagem"
             name="imageUrl"
             id="imageUrl"
+            value={profile.imageUrl}
+            onChange={handleChangeValues}
             required
           />
 
@@ -32,10 +104,12 @@ const EditProfile = () => {
             placeholder=" Nome do perfil"
             name="title"
             id="title"
+            value={profile.title}
+            onChange={handleChangeValues}
             required
           />
 
-          <Style.EditProfileBtn>Atualizar</Style.EditProfileBtn>
+          <Style.EditProfileBtn type="submit">Atualizar</Style.EditProfileBtn>
         </Style.EditForm>
       </ContainerForm>
     </Style.Background>
