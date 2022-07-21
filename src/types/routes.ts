cf8a1314f51897e651f1cc439908dc706f7a6ba7
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-21 16:39:22 -0300

    feat(routes): implement id in HOMEPAGE and GAMES_LIST routes

--- src/types/routes.ts
+++ src/types/routes.ts
@@ -5,7 +5,7 @@ export enum RoutesPath {
     PROFILES = '/profiles',
     CREATE_PROFILES = '/profiles/create',
     EDIT_PROFILE = '/profiles/edit/:id',
-    HOMEPAGE = '/profile/homepage',
-    GAMES_LIST = '/profile/homepage/list',
+    HOMEPAGE = '/profile/homepage/:id',
+    GAMES_LIST = '/profile/homepage/list/:id',
     CREATE_GAMES_AND_GENRER = '/profile/createGames&Genrer'
 }
