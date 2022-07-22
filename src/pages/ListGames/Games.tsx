c998a72e866b8ebc4a01b55b455fae47adcef5cb
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-21 22:30:17 -0300

    fix(GamesList): cannot take games from profileId request

--- src/pages/ListGames/Games.tsx
+++ src/pages/ListGames/Games.tsx
@@ -1,23 +1,28 @@
 import * as Style from "./Games-style";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ReturnPage from "components/ReturnPage";
 import SearchGames from "components/Search/Games";
 import GenrerOption from "components/GenrerOption/GenrerOption";
 import CardGames from "components/CardGames/List";
 import { ChangeEvent, useEffect, useState } from "react";
-import { GamesTypes } from "types/interfaces";
+import { GamesTypes, GamesProfile } from "types/interfaces";
 import { GetGames } from "Service/gamesService";
+import { HomepageProfile } from "Service/homepageService";
 
 const GamesList = () => {
+  const {id} = useParams()
   const navigate = useNavigate();
 
-  const [games, setGames] = useState<GamesTypes[]>([]);
+  const [games, setGames] = useState<GamesProfile[]>([]);
   const [search, setSearch] = useState<string>("");
 
   useEffect(() => {
     const fetchGames = async () => {
-      const payload: any = await GetGames.AllGames();
-      setGames(payload.data);
+      if (id) {
+        const payload = await HomepageProfile.GetHomepageByProfileId(id);
+        console.log(payload?.data)
+        setGames(payload?.data.profileData.games);
+      }
     };
     fetchGames();
   }, []);
@@ -28,7 +33,7 @@ const GamesList = () => {
 
   return (
     <Style.GamesList>
-      <ReturnPage Route={() => navigate("/profile/homepage")} />
+      <ReturnPage Route={() => navigate(`/profile/homepage/${id}`)} />
 
       <Style.ListSection>
         <SearchGames value={search} handleSearchValue={handleSearchValue} />
@@ -41,10 +46,10 @@ const GamesList = () => {
         <GenrerOption />
 
         <Style.ContainerCard>
-          {search !== ""
+          {/* {search !== ""
             ? games
                 .filter(
-                  (e: GamesTypes) =>
+                  (e: GamesProfile) =>
                     e.title.toLowerCase().includes(search.toLowerCase()) ||
                     e.year.toFixed().includes(search.toLowerCase()) ||
                     e.imbScore.toFixed().includes(search.toLowerCase())
@@ -72,7 +77,7 @@ const GamesList = () => {
                   <p>{game.year}</p>
                   <p>{game.imbScore}</p>
                 </CardGames>
-              ))}
+              ))} */}
         </Style.ContainerCard>
       </Style.ListSection>
     </Style.GamesList>
