db6c87da290764c436049bb051b785e5acb805dc
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-22 01:47:46 -0300

    feat(genrerService): take all requests from api

--- src/Service/genrerService.ts
+++ src/Service/genrerService.ts
@@ -1,10 +1,67 @@
 import Api from "./api";
 import swal from "sweetalert";
+import { CreateGenrerType } from "types/interfaces";
 
 export const Genrers = {
   AllGenrers: async () => {
     try {
       const res = await Api.get("/genders", { method: "GET" });
+      return res;
+    } catch (error: any) {
+      swal({
+        title: "Error",
+        text: `${error.message}`,
+        icon: "error",
+        timer: 6000,
+      });
+    }
+  },
+
+  GetGenrerById: async (id: string) => {
+    try {
+      const res = await Api.get(`/genders/${id}`);
+      return res;
+    } catch (error: any) {
+      swal({
+        title: "Error",
+        text: `${error.message}`,
+        icon: "error",
+        timer: 6000,
+      });
+    }
+  },
+
+  CreateGenrer: async (genrer: CreateGenrerType) => {
+    try {
+      const res = await Api.post("/genders", genrer);
+      return res;
+    } catch (error: any) {
+      swal({
+        title: "Error",
+        text: `${error.message}`,
+        icon: "error",
+        timer: 6000,
+      });
+    }
+  },
+
+  EditGenrer: async (id: string, genrer: CreateGenrerType) => {
+    try {
+      const res = await Api.patch(`/genders/${id}`, genrer);
+      return res;
+    } catch (error: any) {
+      swal({
+        title: "Error",
+        text: `${error.message}`,
+        icon: "error",
+        timer: 6000,
+      });
+    }
+  },
+
+  DeleteGenrer: async (id: string) => {
+    try {
+      const res = await Api.delete(`/genders/${id}`);
       return res;
     } catch (error: any) {
       swal({
