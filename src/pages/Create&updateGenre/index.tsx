d4151f540028531452a35c096abf45133268d519
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-22 17:44:21 -0300

    feat(GenrerPage): implement page and styles

--- src/pages/Create&updateGenre/index.tsx
+++ src/pages/Create&updateGenre/index.tsx
@@ -1,8 +1,90 @@
-import React from 'react'
+import ReturnPage from 'components/ReturnPage'
+import * as Style from './style'
+import { useNavigate } from 'react-router-dom'
+import ContainerForm from 'components/ContainerForm';
+import React, { useState } from 'react';
+import { CreateGenrerType } from 'types/interfaces';
+import { Genrers } from 'Service/genrerService';
+import swal from "sweetalert";
 
 const CrudGenrer = () => {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState<CreateGenrerType>({
+    name: '',
+  });
+
+  const handleValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName((name: CreateGenrerType) => ({
+      ...name,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const createGenrer = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const payloadCreate = await Genrers.CreateGenrer(name);
+
+    if (payloadCreate) {
+      swal({
+        title: "Gênero cadastrado com sucesso!",
+        icon: "success",
+        timer: 6000,
+      });
+    }
+    else {
+      swal({
+        title: "Error ao cadastrar gênero!",
+        icon: "error",
+        timer: 6000,
+      });
+    }
+  }
+
   return (
-    <div>CrudGenrer</div>
+    <Style.Page>
+      <ReturnPage Route={() => navigate(-1)} />
+
+      <Style.DivContainers>
+        <ContainerForm>
+          <Style.DescriptionForm>Adicionar gêneros de jogos</Style.DescriptionForm>
+
+          <Style.Form onSubmit={createGenrer}>
+            <Style.Input 
+              type='text'
+              placeholder=' Gênero'
+              name='name'
+              id='name'
+              onChange={handleValue}
+              required
+            />
+
+            <Style.Btn type='submit'>Adicionar</Style.Btn>
+          </Style.Form>
+        </ContainerForm>
+
+        <ContainerForm>
+          <Style.DescriptionForm>Editar ou excluir gêneros</Style.DescriptionForm>
+
+          <Style.Form>
+
+            <Style.ContainerOption>
+              <Style.OptionSelect>
+                <Style.Options value='default'>
+                  Gênero
+                </Style.Options>
+                <Style.Options value=''>Teste</Style.Options>
+              </Style.OptionSelect>
+            </Style.ContainerOption>
+
+            <Style.Input 
+              
+            />
+          </Style.Form>
+        </ContainerForm>
+      </Style.DivContainers>
+    </Style.Page>
   )
 }
 
--- src/pages/Create&updateGenre/style.ts
+++ src/pages/Create&updateGenre/style.ts
@@ -1,2 +1,109 @@
 import styled, { css } from "styled-components";
 
+export const Page = styled.main`
+  ${({ theme }) => css`
+    ${theme.mixins.BackgroundProfiles};
+    background-image: ${theme.constants.homepageBackground};
+    background-repeat: no-repeat;
+    background-size: 100%;
+    filter: grayscale(50%);
+    min-width: 100%;
+    min-height: 100%;
+    cursor: default;
+    ::selection {
+      background: transparent;
+      color: ${theme.colors.textColor};
+    }
+  `}
+`;
+
+export const DivContainers = styled.div`
+  ${() => css`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: auto;
+    grid-column-gap: 2rem;
+  `}
+`;
+
+export const DescriptionForm = styled.h2`
+    ${({theme}) => css`
+        ${theme.mixins.subTitleStyle}
+        margin: 1rem auto;
+    `}
+`;
+
+export const Form = styled.form`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `}
+`;
+
+export const Input = styled.input`
+    ${({theme}) => css`
+        width: 395px;
+        height: 40px;
+        border-radius: 15px;
+        border-color: #000;
+        outline: none;
+        margin: 1rem auto;
+        box-shadow: 0 4px rgba(0, 0, 0, .4);
+        ${theme.mixins.bodyStyle}
+    `}
+`;
+
+export const Btn = styled.button`
+    ${({theme}) => css`
+        ${theme.mixins.loginButton}
+        color: #000;
+    `}
+`;
+
+export const ContainerOption = styled.div`
+    ${() => css`
+        display: flex;
+        align-items: center;
+        width: fit-content;
+        margin: 1rem auto;
+    `}
+`;
+
+export const OptionSelect = styled.select`
+    ${() => css`
+        width: 260px;
+        height: auto;
+        text-align: center;
+        font-size: 1.2rem;
+        color: #f4f5f9;
+        background-color: rgba(0, 0, 0, .6);
+        border: 2px solid rgba(0, 0, 0, .6);
+        outline: none;
+        border-radius: 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+    `}
+`;
+
+export const Options = styled.option`
+    ${() => css`
+        background-color: rgba(0, 0, 0, .9);
+        color: #f4f5f9;
+        font-weight: bold;
+    `}
+`;
+
+export const GenrerOption = styled.div`
+    ${() => css`
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        margin-top: 75px;
+        gap: 75px 20px;
+    `}
+`
