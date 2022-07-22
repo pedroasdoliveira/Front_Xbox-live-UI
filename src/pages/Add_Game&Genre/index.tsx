8f7e5803e5fe7d428846cb81badf8169eb2dd7ae
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-22 01:44:33 -0300

    feat(CreateGames): implement modifications

--- src/pages/Add_Game&Genre/index.tsx
+++ src/pages/Add_Game&Genre/index.tsx
@@ -1,14 +1,143 @@
-import { useNavigate } from 'react-router-dom'
-import * as Style from './style'
+import ContainerForm from "components/ContainerForm";
+import ReturnPage from "components/ReturnPage";
+import swal from "sweetalert";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { GetGames } from "Service/gamesService";
+import { CreateGameType } from "types/interfaces";
+import * as Style from "./style";
 
 const CrudGamesGenrer = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const [game, setGame] = useState<CreateGameType>({
+    title: "",
+    coverImageUrl: "",
+    description: "",
+    year: 0,
+    imbScore: 0,
+    gameplayYouTubeUrl: "",
+    trailerYoutubeUrl: "",
+    genreGame: "",
+  });
+
+  const handleValues = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGame((value: CreateGameType) => ({
+      ...value,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleCreateGame = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const payloadGame = await GetGames.CreateGame(game)
+
+    if (payloadGame) {
+      swal({
+        title: "Jogo cadastrado com sucesso!",
+        icon: "success",
+        timer: 6000,
+      });
+      console.log(payloadGame.data)
+    }
+    else {
+      swal({
+        title: "Error ao cadastrar o jogo",
+        icon: "error",
+        timer: 6000,
+      });
+      console.log(payloadGame)
+    }
+  }
 
   return (
     <Style.Page>
-      <Style.BackIcon onClick={() => navigate('/profile/homepage')} />
-    </Style.Page>
-  )
-}
+      <ReturnPage Route={() => navigate("/profiles")} />
 
-export default CrudGamesGenrer
+      <ContainerForm>
+        <Style.CoverImgGame
+          src={game.coverImageUrl ? game.coverImageUrl : ""}
+        />
+
+        <Style.Form onSubmit={handleCreateGame}>
+          <Style.InputCreate
+            type="text"
+            placeholder="Coloque o nome do jogo (de preferencia sem ser exclusivo)"
+            name="title"
+            id="title"
+            onChange={handleValues}
+            required
+          />
+
+          <Style.InputCreate
+            type="text"
+            placeholder="Url da capa do jogo"
+            name="coverImageUrl"
+            id="coverImageUrl"
+            onChange={handleValues}
+            required
+          />
+
+          <Style.InputCreate
+            type="text"
+            placeholder="Descrição breve sobre o jogo"
+            name="description"
+            id="description"
+            onChange={handleValues}
+            required
+          />
+
+          <Style.InputCreate
+            type="number"
+            placeholder="Ano de lançamento"
+            name="year"
+            id="year"
+            onChange={handleValues}
+            required
+          />
+
+          <Style.InputCreate
+            type="number"
+            placeholder="ImbScore do jogo (de 0 a 5)"
+            name="imbScore"
+            id="imbScore"
+            onChange={handleValues}
+            required
+          />
+
+          <Style.InputCreate
+            type="text"
+            placeholder="Url do trailer do jogo"
+            name="trailerYoutubeUrl"
+            id="trailerYoutubeUrl"
+            onChange={handleValues}
+            required
+          />
+
+          <Style.InputCreate
+            type="text"
+            placeholder="Url de gameplay"
+            name="gameplayYouTubeUrl"
+            id="gameplayYouTubeUrl"
+            onChange={handleValues}
+            required
+          />
+
+          <Style.InputCreate
+            type="text"
+            placeholder="Tipo de gênero/categoria do jogo"
+            name="genreGame"
+            id="genreGame"
+            onChange={handleValues}
+            required
+          />
+
+          <Style.CreateBtn type='submit'>Cadastrar jogo</Style.CreateBtn>
+        </Style.Form>
+      </ContainerForm>
+    </Style.Page>
+  );
+};
+
+export default CrudGamesGenrer;
--- src/pages/Add_Game&Genre/style.ts
+++ src/pages/Add_Game&Genre/style.ts
@@ -1,23 +1,57 @@
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Page = styled.main`
+  ${({ theme }) => css`
+    ${theme.mixins.BackgroundProfiles};
+    background-image: ${theme.constants.homepageBackground};
+    background-repeat: no-repeat;
+    background-size: 100%;
+    filter: grayscale(50%);
+    min-width: 100%;
+    min-height: 100%;
+    cursor: default;
+    ::selection {
+      background: transparent;
+      color: ${theme.colors.textColor};
+    }
+  `}
+`;
+
+export const CoverImgGame = styled.img`
+  ${() => css`
+    width: 206px;
+    height: 206px;
+    border: 1px solid #0d1522;
+    border-radius: 50%;
+    cursor: pointer;
+  `}
+`;
+
+export const Form = styled.form`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `}
+`;
+
+export const InputCreate = styled.input`
     ${({theme}) => css`
-        background-image: ${theme.constants.homepageBackground};
-        background-repeat: no-repeat;
-        background-size: cover;
-        filter: grayscale(50%);
-        min-width: 100vw;
-        min-height: 100vh;
-        cursor: default;
-        ::selection {
-            background: transparent;
-            color: ${theme.colors.textColor};
-        }
+        width: 395px;
+        height: 40px;
+        border-radius: 15px;
+        border-color: #000;
+        outline: none;
+        margin: .6rem auto;
+        box-shadow: 0 4px rgba(0, 0, 0, .4);
+        ${theme.mixins.bodyStyle}
     `}
 `;
 
-export const BackIcon = styled.i`
+export const CreateBtn = styled.button`
     ${({theme}) => css`
-        ${theme.mixins.BackIcon}
+        ${theme.mixins.loginButton}
+        color: #000;
     `}
 `
