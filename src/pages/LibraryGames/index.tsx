586a0c9dbae6e5a3a022e4a81e2cb0a3cf095e36
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-21 22:28:15 -0300

    feat(LibraryGames): implement components and styles

---
+++ src/pages/LibraryGames/index.tsx
@@ -0,0 +1,84 @@
+import CardGames from "components/CardGames/List";
+import GenrerOption from "components/GenrerOption/GenrerOption";
+import ReturnPage from "components/ReturnPage";
+import SearchGames from "components/Search/Games";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { GetGames } from "Service/gamesService";
+import { GamesTypes } from "types/interfaces";
+import * as Style from "./style";
+
+const LibraryAllGames = () => {
+  const navigate = useNavigate();
+
+  const [games, setGames] = useState<GamesTypes[]>([]);
+  const [search, setSearch] = useState<string>("");
+
+  useEffect(() => {
+    getAllGames();
+  }, []);
+
+  const getAllGames = async () => {
+    const payloadGames = await GetGames.AllGames();
+    setGames(payloadGames?.data);
+  };
+
+  const searchGames = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <Style.LibraryGames>
+      <ReturnPage Route={() => navigate("/profiles")} />
+
+      <Style.GamesSection>
+        <SearchGames value={search} handleSearchValue={searchGames} />
+
+        <Style.Categorie>
+          <Style.SectionSearch>
+            Todos os jogos disponiveis na plataforma
+          </Style.SectionSearch>
+        </Style.Categorie>
+
+        <GenrerOption />
+
+        <Style.ContainerCard>
+          {search !== ""
+            ? games
+                .filter(
+                  (e: GamesTypes) =>
+                    e.title.toLowerCase().includes(search.toLowerCase()) ||
+                    e.year.toFixed().includes(search.toLowerCase()) ||
+                    e.imbScore.toFixed().includes(search.toLowerCase())
+                )
+                .map((game) => (
+                  <CardGames key={game.id} title={game.title}>
+                    <Style.CoverImageGame src={game.coverImageUrl} alt="" />
+                    <Style.GenrerGame>
+                      {game.genders.map((genrer) => (
+                        <span key={genrer.id}>{genrer.name}</span>
+                      ))}
+                    </Style.GenrerGame>
+                    <p>{game.year}</p>
+                    <p>{game.imbScore}</p>
+                  </CardGames>
+                ))
+            : games.map((game) => (
+                <CardGames key={game.id} title={game.title}>
+                  <Style.CoverImageGame src={game.coverImageUrl} alt="" />
+                  <Style.GenrerGame>
+                    {game.genders.map((genrer) => (
+                      <span key={genrer.id}>{genrer.name}</span>
+                    ))}
+                  </Style.GenrerGame>
+                  <p>{game.year}</p>
+                  <p>{game.imbScore}</p>
+                </CardGames>
+              ))}
+        </Style.ContainerCard>
+      </Style.GamesSection>
+    </Style.LibraryGames>
+  );
+};
+
+export default LibraryAllGames;
---
+++ src/pages/LibraryGames/style.ts
@@ -0,0 +1,75 @@
+import styled, { css } from "styled-components";
+
+export const LibraryGames = styled.main`
+  ${({ theme }) => css`
+    display: grid;
+    justify-items: center;
+    background-image: ${theme.constants.homepageBackground};
+    background-repeat: no-repeat;
+    background-size: cover;
+    filter: grayscale(50%);
+    min-width: 100vw;
+    min-height: 100vh;
+    cursor: default;
+    flex-direction: column;
+    ${theme.mixins.bodyStyle}
+  `}
+`;
+
+export const GamesSection = styled.section`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: auto;
+    margin: 2rem;
+    padding: 2rem;
+  `}
+`;
+
+export const Categorie = styled.div`
+  ${() => css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    margin: 1rem 0;
+  `}
+`;
+
+export const SectionSearch = styled.p`
+  ${({ theme }) => css`
+    ${theme.mixins.subTitleStyle}
+    margin: 0 3rem;
+    color: #f4f5f9;
+    cursor: pointer;
+  `}
+`;
+
+export const ContainerCard = styled.section`
+  ${() => css`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: auto;
+    grid-column-gap: 1rem;
+    grid-row-gap: 1rem;
+  `}
+`;
+
+export const CoverImageGame = styled.img`
+  ${() => css`
+    width: 320px;
+    height: 400px;
+    margin: 0 auto;
+    border-radius: 10px;
+  `}
+`;
+
+export const GenrerGame = styled.div`
+  ${() => css`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    margin-top: 0.5rem;
+  `}
+`;
