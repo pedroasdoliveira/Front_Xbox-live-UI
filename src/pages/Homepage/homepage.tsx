9c39c4442dbc7451a333b5316b830a6cbefb3318
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-21 22:27:40 -0300

    fix(homepage): FavoriteGames is not take games

--- src/pages/Homepage/homepage.tsx
+++ src/pages/Homepage/homepage.tsx
@@ -1,11 +1,11 @@
 import CreateGamesAdmin from "components/Create/CreateGames";
 import ReturnPage from "components/ReturnPage";
-import { Games } from "Mocks/Games";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { FavoriteGamesType } from "types/interfaces";
 import { HomepageProfile } from 'Service/homepageService'
 import * as Style from "./homepage-style";
+import { Favorite } from "Service/favoriteService";
 
 const Homepage = () => {
   const {id} = useParams()
@@ -19,11 +19,24 @@ const Homepage = () => {
 
   const profileFavoriteGames = async () => {
     if (id) {
-      const payloadFavorite = await HomepageProfile.HomeByProfileId(id)
-      console.log(payloadFavorite?.data);
-      setFavoriteGames(payloadFavorite?.data.profileData.favoriteGames);
+      await Favorite.GetFavoriteByProfileId(id)
+      .then(data => setFavoriteGames(pre => ({...pre, games: data || []})));
+      // setFavoriteGames({games: [payloadFavorite?.data.profileData.favoriteGames]});
+      // console.log(payloadFavorite?.data.profileData.favoriteGames.games[0]);
+      console.log('Esto aqui', favoriteGames)
     }
   }
+
+  // const favGames = () => {
+  //   if (favoriteGames == undefined) {
+  //     return false
+  //   }
+  //   else {
+  //     console.log('Esto aqui', favoriteGames)
+  //   }
+  // }
+  // favGames();
+  
 
   return (
     <Style.Homepage>
@@ -32,20 +45,15 @@ const Homepage = () => {
       <CreateGamesAdmin />
 
       <Style.CardSection>
-        {/* {favoriteGames.map((favorite, index) => ( // Erro: favoriteGames is not function
-          <div key={index}>
-            {favorite.games.map((game, index) => (
-              <Style.CardGame key={index}>
-                <Style.TitleGame>{game.title}</Style.TitleGame>
-              </Style.CardGame>
-            ))}
-          </div>
-        ))} */}
+          {/* {const gameFav = favoriteGames.filter(games => games.favoriteGames.games)} */}
       </Style.CardSection>
 
       <Style.InfoSection>
-        <Style.InfoCard onClick={() => navigate("/profile/homepage/list")}>
+        <Style.InfoCard onClick={() => navigate(`/profile/homepage/list/${id}`)}>
           <p>Minha biblioteca</p>
+        </Style.InfoCard>
+        <Style.InfoCard onClick={() => navigate(`/profile/homepage/library`)}>
+          <p>Ver tudo disponivel</p>
         </Style.InfoCard>
       </Style.InfoSection>
     </Style.Homepage>
