fa13efbb7c34040b98238b525008bfb12ff89de0
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-25 16:22:31 -0300

    fix(EditGame): implement page and style & error on Updated

--- src/pages/Edit_Game/index.tsx
+++ src/pages/Edit_Game/index.tsx
@@ -1,20 +1,189 @@
-import ContainerForm from 'components/ContainerForm'
-import ReturnPage from 'components/ReturnPage'
-import { useNavigate } from 'react-router-dom'
-import * as Style from './style'
+import ContainerForm from "components/ContainerForm";
+import ReturnPage from "components/ReturnPage";
+import swal from "sweetalert";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { GetGames } from "Service/gamesService";
+import { CreateGameType, GamesTypes } from "types/interfaces";
+import * as Style from "./style";
 
 const EditGameId = () => {
-    const navigate = useNavigate()
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [gameInfo, setGameInfo] = useState<CreateGameType>({
+    title: "",
+    description: "",
+    coverImageUrl: "",
+    year: 0,
+    imbScore: 0,
+    trailerYoutubeUrl: "",
+    gameplayYouTubeUrl: "",
+    genreGame: "",
+  });
+
+  useEffect(() => {
+    GameId();
+  }, []);
+
+  const GameId = async () => {
+    if (id) {
+      const payloadId = await GetGames.GameById(id);
+
+      const gameInfosValues = {
+        title: payloadId?.data.title,
+        description: payloadId?.data.description,
+        coverImageUrl: payloadId?.data.coverImageUrl,
+        year: payloadId?.data.year,
+        imbScore: payloadId?.data.imbScore,
+        trailerYoutubeUrl: payloadId?.data.trailerYoutubeUrl,
+        gameplayYouTubeUrl: payloadId?.data.gameplayYouTubeUrl,
+        genreGame: payloadId?.data.genders[0].name,
+      };
+
+      console.log(payloadId?.data);
+      console.log("Aqui" + gameInfosValues.genreGame);
+      setGameInfo({
+        ...gameInfo,
+        ...gameInfosValues,
+      });
+    }
+  };
+
+  const handleChangesValues = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGameInfo((values: CreateGameType) => ({
+      ...values,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleEditGameInfos = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (id) {
+      const payloadEdit = await GetGames.EditGame(id, gameInfo);
+
+      if (payloadEdit) {
+        swal({
+          title: "Jogo editado com sucesso!",
+          icon: "success",
+          timer: 6000,
+        });
+        navigate(-1);
+      } else {
+        swal({
+          title: "Error",
+          text: "Erro ao atualizar o jogo!",
+          icon: "error",
+          timer: 6000,
+        });
+        console.error(payloadEdit);
+      }
+    }
+  };
 
   return (
-    <Style.MainPage>
-        <ReturnPage Route={() => navigate(-1)}/>
+    <Style.MainPage
+      style={{
+        backgroundImage: `url(${gameInfo?.coverImageUrl})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <ReturnPage Route={() => navigate(-1)} />
 
-        <ContainerForm>
-            
-        </ContainerForm>
+      <ContainerForm>
+        <Style.CoverImgGame
+          src={gameInfo.coverImageUrl ? gameInfo.coverImageUrl : ""}
+        />
+
+        <Style.Form onSubmit={handleEditGameInfos}>
+          <Style.Inputs
+            type="text"
+            placeholder="Nome do jogo"
+            name="title"
+            id="title"
+            value={gameInfo.title}
+            onChange={handleChangesValues}
+            required
+          />
+
+          <Style.Inputs
+            type="text"
+            placeholder="Capa do jogo (1920X1080)"
+            name="coverImageUrl"
+            id="coverImageUrl"
+            value={gameInfo.coverImageUrl}
+            onChange={handleChangesValues}
+            required
+          />
+
+          <Style.Inputs
+            type="text"
+            placeholder="Descrição breve sobre o jogo"
+            name="description"
+            id="description"
+            value={gameInfo.description}
+            onChange={handleChangesValues}
+            required
+          />
+
+          <Style.Inputs
+            type="number"
+            placeholder="Ano do lançamento"
+            name="year"
+            id="year"
+            value={gameInfo.year}
+            onChange={handleChangesValues}
+            required
+          />
+
+          <Style.Inputs
+            type="number"
+            placeholder="Score do jogo (0 até 5)"
+            name="imbScore"
+            id="imbScore"
+            value={gameInfo.imbScore}
+            onChange={handleChangesValues}
+            required
+          />
+
+          <Style.Inputs
+            type="text"
+            placeholder="Trailer do jogo"
+            name="trailerYoutubeUrl"
+            id="trailerYoutubeUrl"
+            value={gameInfo.trailerYoutubeUrl}
+            onChange={handleChangesValues}
+            required
+          />
+
+          <Style.Inputs
+            type="text"
+            placeholder="Gameplay do jogo"
+            name="gameplayYouTubeUrl"
+            id="gameplayYouTubeUrl"
+            value={gameInfo.gameplayYouTubeUrl}
+            onChange={handleChangesValues}
+            required
+          />
+
+          <Style.Inputs
+            type="text"
+            placeholder="Gênero do jogo"
+            name="genreGame"
+            id="genreGame"
+            value={gameInfo.genreGame}
+            onChange={handleChangesValues}
+            required
+          />
+
+          <Style.EditBtn type="submit">Atualizar</Style.EditBtn>
+        </Style.Form>
+      </ContainerForm>
     </Style.MainPage>
-  )
-}
+  );
+};
 
-export default EditGameId
+export default EditGameId;
--- src/pages/Edit_Game/style.ts
+++ src/pages/Edit_Game/style.ts
@@ -16,3 +16,42 @@ export const MainPage = styled.main`
     
   `}
 `;
+
+export const CoverImgGame = styled.img`
+  ${() => css`
+    width: 206px;
+    height: 206px;
+    border: 1px solid #0d1522;
+    border-radius: 50%;
+    cursor: pointer;
+  `}
+`;
+
+export const Form = styled.form`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `}
+`;
+
+export const Inputs = styled.input`
+    ${({theme}) => css`
+        width: 395px;
+        height: 40px;
+        border-radius: 15px;
+        border-color: #000;
+        outline: none;
+        margin: .6rem auto;
+        box-shadow: 0 4px rgba(0, 0, 0, .4);
+        ${theme.mixins.bodyStyle}
+    `}
+`;
+
+export const EditBtn = styled.button`
+    ${({theme}) => css`
+        ${theme.mixins.loginButton}
+        color: #000;
+    `}
+`
