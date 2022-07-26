433373a7e97473d03637d51ffeb9e674c40170c2
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-26 01:37:01 -0300

    feat(favoriteService): implement requests

--- src/Service/favoriteService.ts
+++ src/Service/favoriteService.ts
@@ -1,11 +1,26 @@
 import Api from "./api";
 import swal from "sweetalert";
+import { AddFavorite } from "types/interfaces";
 
 export const Favorite = {
   GetFavoriteByProfileId: async (id: string) => {
     try {
       const res = await Api.get(`/favorites/${id}`);
-      return res
+      return res;
+    } catch (error: any) {
+      swal({
+        title: "Error",
+        text: `${error.message}`,
+        icon: "error",
+        timer: 6000,
+      });
+    }
+  },
+
+  FavoriteGame: async (id: string, favorite: AddFavorite) => {
+    try {
+      const res = await Api.patch(`/favorites/${id}`, favorite);
+      return res;
     } catch (error: any) {
       swal({
         title: "Error",
