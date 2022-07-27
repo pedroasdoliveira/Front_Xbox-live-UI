7e369623491a22b77568ab7e193ca0be5365b234
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-27 14:41:38 -0300

    feat(userConfigs): implement inputs and prettier

--- src/pages/UserConfigs/index.tsx
+++ src/pages/UserConfigs/index.tsx
@@ -1,65 +1,89 @@
-import ContainerForm from 'components/ContainerForm';
-import ReturnPage from 'components/ReturnPage'
-import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom'
-import { User } from 'Service/userService';
-import { UserType, GetUserType } from 'types/interfaces'
-import * as Style from './style'
+import ContainerForm from "components/ContainerForm";
+import ReturnPage from "components/ReturnPage";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { User } from "Service/userService";
+import { UserType, GetUserType } from "types/interfaces";
+import * as Style from "./style";
 
 const UserConfigs = () => {
-    const navigate = useNavigate();
-    const {id} = useParams();
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-    const [user, setUser] = useState<GetUserType>({
-      name: '',
-      nickname: '',
-      email: '',
-      cpf: '',
-      password: '',
-      isAdmin: false,
-    });
+  const [user, setUser] = useState<GetUserType>({
+    name: "",
+    nickname: "",
+    email: "",
+    cpf: "",
+    password: "",
+    isAdmin: false,
+  });
 
-    useEffect(() => {
-      getUserId()
-    }, [])
+  useEffect(() => {
+    getUserId();
+  }, []);
 
-    const getUserId = async () => {
-      if (id) {
-        const payloadUser = await User.GetUserById(id)
-        console.log(payloadUser?.data)
+  const getUserId = async () => {
+    if (id) {
+      const payloadUser = await User.GetUserById(id);
+      console.log(payloadUser?.data);
 
-        const userDatas = {
-          name: payloadUser?.data.name,
-          nickname: payloadUser?.data.nickname,
-          email: payloadUser?.data.email,
-          cpf: payloadUser?.data.cpf,
-          password: payloadUser?.data.password,
-          isAdmin: payloadUser?.data.isAdmin,
-        }
+      const userDatas = {
+        name: payloadUser?.data.name,
+        nickname: payloadUser?.data.nickname,
+        email: payloadUser?.data.email,
+        cpf: payloadUser?.data.cpf,
+        password: payloadUser?.data.password,
+        isAdmin: payloadUser?.data.isAdmin,
+      };
 
-        setUser({
-          ...user,
-          ...userDatas,
-        })
-      }
+      setUser({
+        ...user,
+        ...userDatas,
+      });
     }
+  };
 
   return (
     <Style.Page>
-        <ReturnPage Route={() => navigate(-1)} /> 
+      <ReturnPage Route={() => navigate(-1)} />
 
-        <ContainerForm>
-            <Style.Info>Gerenciamento de usuários</Style.Info>
+      <ContainerForm>
+        <Style.Info>Gerenciamento de usuário</Style.Info>
 
-            <Style.Form>
-              <Style.Inputs 
-                type='text'
-                
-              />
-            </Style.Form>
-        </ContainerForm>
+        <Style.Form>
+          <Style.Inputs
+            type="text"
+            placeholder=" Nome do usuário"
+            name="name"
+            id="name"
+            value={user.name}
+            required
+          />
+
+          <Style.Inputs
+            type="text"
+            placeholder=" Nickname do usuário"
+            name="nickname"
+            id="nickname"
+            value={user.nickname}
+            required
+          />
+
+          <Style.Inputs
+            type="text"
+            placeholder=" Email do usuário"
+            name="email"
+            id="email"
+            value={user.email}
+            required
+          />
+
+          <Style.Btn>Editar</Style.Btn>
+        </Style.Form>
+      </ContainerForm>
     </Style.Page>
-  )
-}
+  );
+};
 
-export default UserConfigs
+export default UserConfigs;
