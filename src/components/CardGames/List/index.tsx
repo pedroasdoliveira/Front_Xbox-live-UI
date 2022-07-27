65c39b6313ee4151ebb2b934ada5cd59898e235b
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-27 14:30:36 -0300

    feat(CardGames): implement favorite state and function

--- src/components/CardGames/List/index.tsx
+++ src/components/CardGames/List/index.tsx
@@ -1,19 +1,43 @@
 import * as Style from './style'
-import { MdFavoriteBorder } from 'react-icons/md'
-import { ReactNode } from 'react';
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { ReactNode, useState } from 'react';
+import { Favorite } from 'Service/favoriteService';
+import { useNavigate } from 'react-router-dom';
 
 interface CardProps {
     title: string;
     children: ReactNode;
     Route: () => void;
-    Favorite: () => void;
+    idGame: string | undefined;
 }
 
 const CardGames = (props: CardProps) => {
+  const navigate = useNavigate()
+
+  const [favorite, setFavorite] = useState<string>()
+
+  const favoriteGame = async () => {
+    const id = localStorage.getItem('profileId')
+
+    if (id) {
+      const favoriteObj = {
+        gameId: props.idGame,
+        favoriteGameId: props.idGame,
+      }
+
+      const payloadFavorite = await Favorite.FavoriteGame(id, favoriteObj);
+      console.log(payloadFavorite?.data);
+      setFavorite(payloadFavorite?.data);
+      navigate(-1)
+    }
+  }
+
   return (
     <Style.Card>
         <Style.IconContainer>
-            <Style.Icon onClick={props.Favorite}><MdFavoriteBorder /></Style.Icon>
+            <Style.Icon onClick={favoriteGame}>
+              {favorite ? <MdFavorite /> : <MdFavoriteBorder />}
+            </Style.Icon>
         </Style.IconContainer>
 
         <Style.TitleGame>{props.title}</Style.TitleGame>
